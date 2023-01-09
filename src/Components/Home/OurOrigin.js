105b53d302ea6d75717cc7b8073b5e00c57f65bf
MAvi-Rup <avirupmondal19@gmail.comm>
2023-01-09 16:33:58 +0600

    Project started

---
+++ src/Components/Home/OurOrigin.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const OurOrigin = () => {
+    return (
+        <div className='container mt-5 mb-5'>
+            <div className='row'>
+                <div className='col-md-3'>
+                    <h1>Who We Are</h1>
+
+                </div>
+                <div className='col-md-9 ps-4 border-start border-primary'>
+                    <p>
+                        Beximco Pharmaceuticals Ltd. (Beximco Pharma) is an emerging generic drug player committed to providing access to affordable medicines. Company’s state-of-the-art manufacturing facilities have been accredited by the regulatory authorities of USA, Australia, European Union, Canada, and Brazil, among others, and it currently focuses on building presence in many emerging and developed markets around the world.</p>
+
+                       <p> Beximco Pharma is consistently building upon its portfolio and currently producing more than 500 products encompassing broad therapeutic categories and the Company has created strong differentiation by offering a range of high-tech, specialized products which are difficult to imitate.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OurOrigin;
