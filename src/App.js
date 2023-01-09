63c9492f917ea37ffa9695ada92a992a4afbbd77
MAvi-Rup <avirupmondal19@gmail.comm>
2023-01-09 17:35:49 +0600

    Home Page Patially Done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Carousel from './Components/Carousel';
 import OurOrigin from './Components/Home/OurOrigin';
+import Research from './Components/Home/Research';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar></Navbar>
       <Carousel></Carousel>
       <OurOrigin></OurOrigin>
+      <Research></Research>
     </div>
   );
 }
---
+++ src/Components/Home/Research.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Research = () => {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-4'>
+                    <div className='ps-5 pe-5'>
+                        <img src="https://i.ibb.co/qNMQskz/logo.png" alt="" />
+                        <h1 className='mb-3 mt-2'>Research & development</h1>
+                        <p>Our product development team continues its strong research orientation in formulation development to transform BPL into a truly international one.</p>
+                        <a href="#">Know about our capabilities</a>
+
+                    </div> 
+
+
+
+                </div>
+                <div className='col-md-4'>
+                    <div className='ps-5 pe-5'>
+                        <img src="https://i.ibb.co/xFN85jM/logo2.png" alt="" />
+                        <h1 className='mb-3 mt-2'>Manufacturing Facilities</h1>
+                        <p>Beximco Pharmaceuticals Ltd. (Beximco Pharma) is a leading manufacturer of medicines and active pharmaceutical ingredients (APIs) based in Bangladesh.</p>
+                        <a href="#">Discover More</a>
+
+                    </div> 
+
+
+
+                </div>
+                <div className='col-md-4'>
+                    <div className='ps-5 pe-5'>
+                        <img src="https://i.ibb.co/wc0Fcj7/logo3.png" alt="" />
+                        <h1 className='mb-3 mt-2'>Our People</h1>
+                        <p>BPL has a dynamic team of over 4,700 people who are the driving force behind its progress.</p>
+                        <a href="#">Meet Our People.</a>
+
+                    </div> 
+
+
+
+                </div>
+
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default Research;
+
+// https://i.ibb.co/qNMQskz/logo.png
+// 
+// 
