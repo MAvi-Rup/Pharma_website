487b845c4ff369f9b50551cdb032248d309ed7c0
Avirup <avirupmondal19@gmail.com>
2023-01-15 18:26:08 +0600

    Done

--- src/Components/Pages/Aboutus.js
+++ src/Components/Pages/Aboutus.js
@@ -2,12 +2,37 @@ import React from 'react';
 
 const Aboutus = () => {
     return (
-        <div className='container-fluid'>
-            <div className='row'>
-                <h1>This is About Us page</h1>
+        <>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-md-6 bg-primary d-flex align-items-center justify-content-end'>
+                        <h1 className='pb-5 text-white'><span className='border-bottom border-light pb-3'>Ab</span>out US</h1>
+                    </div>
+                    <div className='col-md-6'>
+                        <img className='img-fluid' src="./img/About/about.jpg" alt="" />
+
+                    </div>
+
+                </div>
+            </div>
+            <div className='container w-75 m-auto mt-5 pt-5'>
+                <div className='row'>
+                    <div className='col-md-4'>
+                    <h1 className='pb-5'><span className='border-bottom border-primary pb-3'>Our</span> Story</h1>
+
+
+                    </div>
+                    <div className='col-md-8'>
+                        <p className='fs-5'>Beximco Pharmaceuticals Limited is a leading manufacturer and exporter of medicines in Bangladesh. Incorporated in 1976, the Company started its operation by importing products from Bayer, Germany and Upjohn, USA and selling them in the local market. In 1980, Beximco Pharma began manufacturing of these products under licensing arrangement and the company launched its own formulation brands in 1983. From that humble beginning, Beximco Pharma has grown from strength to strength. Today, it has emerged as a global generic pharma company in the region. The Company’s manufacturing facilities have been accredited by the leading global regulatory authorities, and medicines manufactured by the Company are now being exported to more than 50 countries including the highly regulated markets of USA, Europe, Canada and Australia. Beximco has won the National Export (Gold) trophy for 5 times. It remains the only Company in the country to win the highly prestigious SCRIP Award as the “Best Pharma Company in an Emerging Market” and also won CPhI Pharma Awards 2020 for “Innovation in Response to COVID-19.” It also has the unique distinction as the only Bangladeshi Company listed on the AIM of London Stock Exchange.</p>
+                        <p className='fs-5'>In 2018, Beximco Pharma acquired a majority stake in Nuvista Pharma (formerly Organon Bangladesh), a leading hormone and steroid manufacturer in the country.</p>
+                        <p className='fs-5'>The Company currently employs more than 4,700 employees including pharmacists, doctors, engineers, chemists, microbiologists, accountants, business graduates and other white collar professionals.</p>
+
+                    </div>
+
+                </div>
 
             </div>
-        </div>
+        </>
     );
 };
 
--- src/Components/Pages/Investors.js
+++ src/Components/Pages/Investors.js
@@ -2,9 +2,59 @@ import React from 'react';
 
 const Investors = () => {
     return (
-        <div>
-            <h1>Here Our Investor Relation Page.</h1>
+        <>
+         <div className='container-fluid'>
+            <div>
+                <img height='945' className='img-fluid' src="./img/About/investor.jpg" alt="" />
+            </div>
+            
+         </div>
+         <div className='container w-75 m-auto mt-5 pt-5'>
+            <div className='row'>
+                <div className='col-md-4'>
+                <p className='pb-5 fs-1'>Investors <br/><span className='border-bottom border-primary pb-3'>Rel</span>ation</p>
+
+
+                </div>
+                <div className='col-md-8'>
+                    <p className='fs-5'>This section contains important Company information including financial reports, corporate governance, stock exchange announcements and share / GDR holdings etc. for the investors. You can view and download the information of this section. The information are updated at periodic intervals.</p>
+                </div>
+
+            </div>
+
         </div>
+        <div className='container mb-3 mt-5'>
+            <div className='row'>
+                <div className='col-md-4'>
+                    <div className='ps-5 pe-5'>
+                        <img width="238" src="https://www.beximcopharma.com/img/company-information.jpg" alt="" />
+                        <h1 className='mb-3 mt-2 text-primary'>Company Information</h1>
+                        <p>Contains a brief overview of the Company, its line of business, listing with stock exchange and important contact information.</p>
+                        <a className='text-decoration-none' href="#">View Details <img src="https://www.beximcopharma.com/images/right-arrow.svg" alt="" /></a>
+
+                    </div> 
+                </div>
+                <div className='col-md-4'>
+                    <div className='ps-5 pe-5'>
+                        <img width="238" src="https://beximco-pharma.com/cdn/bpl/img/company-documents.jpg" alt="" />
+                        <h1 className='mb-3 mt-2 text-primary'>Company Documents</h1>
+                        <p>Company’s constitutional documents i.e. Memorandum and Article of Association is available here. Additionally, the document relating to Placing and Admission of GDRs to AIM is also available in this section.</p>
+                        <a className='text-decoration-none' href="#">View Details <img src="https://www.beximcopharma.com/images/right-arrow.svg" alt="" /></a>
+
+                    </div> 
+                </div>
+                <div className='col-md-4'>
+                    <div className='ps-5 pe-5'>
+                        <img width="238" src="https://beximco-pharma.com/cdn/bpl/investor/Financial-Reports.jpg" alt="" />
+                        <h1 className='mb-3 mt-2 text-primary'>Research & development</h1>
+                        <p>This section contains Company’s Quarterly, Half Yearly, and Annual reports of different years.</p>
+                        <a className='text-decoration-none' href="#">View Details <img src="https://www.beximcopharma.com/images/right-arrow.svg" alt="" /></a>
+
+                    </div> 
+                </div>
+            </div>
+        </div>
+        </>
     );
 };
 
