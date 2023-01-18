7f2b3000ec700c3f6631f415ad7ad20fdf780ee8
Avirup <avirupmondal19@gmail.com>
2023-01-18 14:05:58 +0600

    Load button problem fixed

--- src/Components/Home/AllProducts.js
+++ src/Components/Home/AllProducts.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import ProductCard from './ProductCard';
 
 const AllProducts = () => {
+    const [isCompleted, setIsCompleted] = useState(false)
     const [allProducts,setProduct]= useState([])
     useEffect(()=>{
         fetch(`http://localhost:5000/allProducts`)
@@ -14,15 +15,22 @@ const AllProducts = () => {
     const navigateProduct = id => {
         navigate(`/allproduct/${id}`)
     }
+    const loadMore = ()=>{
+        setIsCompleted(true)
+    }
     return (
         <div>
             <div className="container pt-5 mb-5">
                 <h1 className='pb-5 text-black'><span className='border-bottom border-primary pb-3'>Our</span>Products</h1>
                 <div className='row pt-5 pb-5'>
                        {
-                        product.map((product=><div className="col-md-3 pe-2 pb-3"><img onClick={()=>{navigateProduct(product._id)}} src={product.img} width='255px' /> </div>))
+                        isCompleted != true ?  product.map((product=><div className="col-md-3 pe-2 pb-3"><img onClick={()=>{navigateProduct(product._id)}} src={product.img} width='255px' /> </div>)): allProducts.map((product=><div className="col-md-3 pe-2 pb-3"><img onClick={()=>{navigateProduct(product._id)}} src={product.img} width='255px' /> </div>))
                        }
-                       {allProducts.length>8 ?<button className='btn btn-primary' onClick={()=>{allProducts.map((product2=><div className="col-md-3 pe-2 pb-3"><img onClick={()=>{navigateProduct(product2._id)}} src={product2.img} width='255px' /> </div>))}}>Load More</button> :<></>}
+                       {/* {
+                        product.map((product=><div className="col-md-3 pe-2 pb-3"><img onClick={()=>{navigateProduct(product._id)}} src={product.img} width='255px' /> </div>))
+                       } */}
+
+                       {isCompleted != true ?<button className='btn btn-primary' onClick={loadMore}>Load More</button> :<button className='btn btn-primary' disabled onClick={loadMore}>Load More</button>}
                 </div>
             </div>
         </div>
