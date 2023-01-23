43a3e1883515d3664ce7461b031926fc060b04a3
MAvi-Rup <avirupmondal19@gmail.comm>
2023-01-23 14:11:13 +0600

    COntact Form Finish

--- src/Components/Pages/ContactUs.js
+++ src/Components/Pages/ContactUs.js
@@ -38,15 +38,24 @@ const ContactUs = () => {
             <div className='row'>
                 <h1 className='fs-4 text-white bg-primary pt-2 pb-2'>Contact Form</h1>
                 <form>
+                <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="exampleInputPassword1" />
+                    </div>
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Email address</label>
                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                        <label for="exampleInputPassword1" class="form-label">Subject</label>
+                        <input type="text" class="form-control" id="exampleInputPassword1" />
                     </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Query</label>
+                        <textarea type="text" class="form-control" id="exampleInputPassword1" />
+                    </div>
+                    
                     <div class="mb-3 form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1" />
                             <label class="form-check-label" for="exampleCheck1">Check me out</label>
